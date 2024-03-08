f196131d7f3cbc266a6003bd9507b9e9db79dae8
ihab abadi <ihab@utopios.net>
2024-03-08 16:09:47 +0100

    exercice 5

--- correction_workshop_1/exercice5.py
+++ correction_workshop_1/exercice5.py
@@ -1,5 +1,5 @@
 reponse_utilisateur = input("Le serveur est-il en maintenance ? (oui/non) : ") == "oui"
 
-message = "En etat" if reponse_utilisateur else "En maintenance"
+message = f"Etat du serveur {reponse_utilisateur}"
 
 print(message)
