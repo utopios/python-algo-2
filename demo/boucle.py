a54bb8cd4bddad9481b89135a3a8762d0f8e3363
ihab abadi <ihab@utopios.net>
2024-03-13 09:35:12 +0100

    avec structure itérative

---
+++ demo/boucle.py
@@ -0,0 +1,28 @@
+## Tant que condition est vérifiée => on execute une action
+# increment = 1
+# while increment < 10:
+#     print("je me répète !!")
+#     increment += 1
+
+# for i in range(1, 11):
+#     print(f"je me répète !!!! {i}")
+
+
+# for i in range(1, 20):
+#     print(f"je me répète !!!! {i}")
+#     if i > 5 and i%2 == 0:
+#         break
+
+
+# for i in range(1, 20):
+#     if i%2 ==0:
+#         print(f"je me répète !!!! {i}")
+#     else:
+#         continue
+
+# for i in range(1,20, 3):
+#     print(f"je me répète !!!! {i}")
+
+
+for i in range(400,200,-3):
+    print(f"je me répète !!!! {i}")
